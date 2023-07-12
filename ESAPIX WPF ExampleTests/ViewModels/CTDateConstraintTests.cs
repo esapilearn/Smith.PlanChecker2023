aa971a0f1b81e6446e51f06077ccb2649cda29d2
Geoff Smith <geoff.smith@outlook.co.nz>
2023-07-12 10:43:29 -0700

    Unit testing on CTDate implemented but not working yet. Application functional

---
+++ ESAPIX WPF ExampleTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESAPX_StarterUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESAPX_StarterUI.ViewModels.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void ConstraintCTDateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
